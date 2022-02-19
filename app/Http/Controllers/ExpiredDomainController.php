29ba8c5cd21b6643ab63656a6d1737c7fb8d735e
adnan <alhossain.a92@gmail.com>
2022-02-19 11:10:38 +0600

    dockerize project and some code indentesion

--- app/Http/Controllers/ExpiredDomainController.php
+++ app/Http/Controllers/ExpiredDomainController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\ExpiredDomain;
 use App\Jobs\ExpiredDomainProcess;
 use App\Imports\FileImport;
@@ -15,8 +14,10 @@ use ExcelReport;
 class ExpiredDomainController extends Controller
 {
 
-public $data;
+    public $data;
     protected $expireddomainservice;
+
+
     public function __construct(ExpiredDomainService $expireddomainservice){
         $this->expireddomainservice=$expireddomainservice;
 
@@ -24,139 +25,86 @@ public $data;
    
     public function index(Request $request){
     
-        if ($request->ajax()) {
-
+        if ($request->ajax()){
             $data = ExpiredDomain::select(['id', 'domain']);
-            // $this->test($data);
-                  
             return Datatables::of($data) 
-
             ->filter(function ($query) use ($request) {
-
                 $fileter_data = $this->expireddomainservice->conditions($request,$query);
-             
             })
             ->addIndexColumn()
             ->addColumn('action', function($row){
 
-                    $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
-                    return $btn;
+                $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+                return $btn;
 
-                    })->rawColumns(['action'])
-                     ->make(true);
+            })->rawColumns(['action'])
+            ->make(true);
                    
-        }
+        }       
 
-         return view('expiredDomain/index');
-       
-       
+        return view('expiredDomain/index');   
     }
 
 
 
 
 
-public function create(){
+    public function create(){   
 
-
-
-      
-    $queryBuilder = ExpiredDomain::select(['id', 'domain']);
-
-    $title = 'Registered User Report'; // Report title
-
-    $meta = [ // For displaying filters description on header
+        $queryBuilder = ExpiredDomain::select(['id', 'domain']);
+        $title = 'Registered User Report'; // Report title
+        $meta = [ // For displaying filters description on header
             'Registered on' =>"this is date"
         ];
 
-    $columns = [ // Set Column to be displayed
+        $columns = [ // Set Column to be displayed
             'ID' => 'id',
             'domain_Name' => 'domain'
         ];
-
         return ExcelReport::of($title, $meta, $queryBuilder, $columns)
         ->limit(20) 
         ->download('adnan');
-}
+    }
 
   
-// public function test($data){
 
-//     return $data;
-
-// }
-
-
-
-    public function store(Request $request)
-    {
-
+    public function store(Request $request){ 
+    
         $file=$request->file('file');
         $extension=$file->extension();
        
         if($extension =="txt"){
 
-        $doc=file_get_contents($file);
+            $doc=file_get_contents($file);
 
-        $domains=explode("\n",$doc);
-        $chunks = array_chunk($domains,1000);
+            $domains=explode("\n",$doc);
+            $chunks = array_chunk($domains,1000);
             $header = [];
 
             foreach ($chunks as $key => $domain) {
 
-            $data = array_map('str_getcsv', $domain);
+                $data = array_map('str_getcsv', $domain);
 
-                if($key == 0){
+                    if($key == 0){
 
-                    $header = $data[0];
-
-                    unset($data[0]);
+                        $header = $data[0];
+                        unset($data[0]);
+                    }
+                ExpiredDomainProcess::dispatch($data,$header); 
                 }
-      ExpiredDomainProcess::dispatch($data,$header); 
-            }
-             echo "stored"; 
+                echo "stored"; 
 
-         }
+        }
 
-         else{
+        else{
 
-       $file=$request->file('file');
-        Excel::import(new FileImport,$file);
+            $file=$request->file('file');
+            Excel::import(new FileImport,$file);
+            $cart=Jobs::count();
+            dd($cart);
 
-
-        $cart=Jobs::count();
-
-        dd($cart);
-
-
-         }
+        }
    
-}
-
-       
-    
-
-   
-    public function show($id)
-    {
-        //
     }
 
-  
-    public function edit($id)
-    {
-        //
-    }
-
-   
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-   
-    public function destroy($id)
-    {
-        //
-    }
 }
--- app/Services/ExpiredDomainService.php
+++ app/Services/ExpiredDomainService.php
@@ -4,31 +4,26 @@ use App\Models\Category;
 use DataTables;
 
 class ExpiredDomainService{
-	public function conditions($request, $query=null,){
-      if(!is_null($query)){
+	public function conditions($request, $query = null){
+    if(!is_null($query)){
 
-      
-		                $keyword=$request->keyword;
-                    if ($request->keyword) {
-                      $filter_data =  $query->where('domain', 'like', "{$keyword}%");
-                      return $filter_data;
-                    }
-                    if($request->number == "only"){
-                     $filter_data =  $query->where('domain','REGEXP',"[0-9]");
-                      return $filter_data;
-                    }
-                    if($request->number == "mixed"){
-                      $filter_data = $query->where('domain','REGEXP',"[a-z|0-9]");
-                      return $filter_data;
-                    }
-                    if($request->dash == "yes"){
-                       $filter_data = $query->where('domain','REGEXP',"[-]");
-return $filter_data;
-                   
-                    }
-                     
-}
+        $keyword=$request->keyword;
+        if ($request->keyword) {
+          $filter_data =  $query->where('domain', 'like', "{$keyword}%");
+          return $filter_data;
+        }
+        if($request->number == "only"){
+          $filter_data =  $query->where('domain','REGEXP',"[0-9]");
+          return $filter_data;
+        }
+        if($request->number == "mixed"){
+          $filter_data = $query->where('domain','REGEXP',"[a-z|0-9]");
+          return $filter_data;
+        }
+        if($request->dash == "yes"){
+            $filter_data = $query->where('domain','REGEXP',"[-]");
+        return $filter_data;
+    }                   }
 	}
 }
-
 ?>
